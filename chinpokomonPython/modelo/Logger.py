2003d1dbf0248a966f86433198ae2b2c1137fde4
trinchero52 <trinchero52@gmail.com>
2022-09-16 10:02:03 -0300

    agrego los estados en el script del logger

--- chinpokomonPython/modelo/Logger.py
+++ chinpokomonPython/modelo/Logger.py
@@ -1,10 +1,91 @@
-from this import d
+
+class Estado(object):
+    "Clase que representa una persona."
+
+    def __init__(self, logger):
+        self._logger = logger
+
+    @property
+    def logger(self):
+        return self._logger
+
+    @logger.setter
+    def logger(self, logger):
+        self._logger = logger
+
+    def info2(self, mensaje):
+        pass
+
+    def warn2(self, mensaje):
+        pass
+
+    def error2(self, mensaje):
+        pass
+
+    def debug2(self, mensaje):
+        pass
+
+
+class Debug(Estado):
+    def info2(self, mensaje):
+        print("INFO: " + mensaje)
+
+    def warn2(self, mensaje):
+        print("WARN: " + mensaje)
+
+    def error2(self, mensaje):
+        print("ERROR: " + mensaje)
+
+    def debug2(self, mensaje):
+        print("DEBUG: " + mensaje)
+
+
+class Error(Estado):
+    def info2(self, mensaje):
+        pass
+
+    def warn2(self, mensaje):
+        pass
+
+    def error2(self, mensaje):
+        print("ERROR: " + mensaje)
+
+    def debug2(self, mensaje):
+        pass
+
+
+class Info(Estado):
+    def info2(self, mensaje):
+        print("INFO: " + mensaje)
+
+    def warn2(self, mensaje):
+        print("WARN: " + mensaje)
+
+    def error2(self, mensaje):
+        print("ERROR: " + mensaje)
+
+    def debug2(self, mensaje):
+        pass
+
+
+class Warn(Estado):
+    def info2(self, mensaje):
+        pass
+
+    def warn2(self, mensaje):
+        print("WARN: " + mensaje)
+
+    def error2(self, mensaje):
+        print("ERROR: " + mensaje)
+
+    def debug2(self, mensaje):
+        pass
 
 
 class SingletonClass(object):
 
-    def __init__(self, estado):
-        self._estado = estado
+    def __init__(self):
+        self._estado = Debug()
 
     @property
     def estado(self):
@@ -40,3 +121,6 @@ print(singleton is new_singleton)
 
 singleton.singl_variable = "Singleton Variable"
 print(new_singleton.singl_variable)
+
+singleton = SingletonClass()
+print(singleton.info("probando"))
