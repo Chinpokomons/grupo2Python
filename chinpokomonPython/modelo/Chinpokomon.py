02fb67dca7399f71ba073f965c67f7e00bf1d784
Tomas Miranda <tomasmiranda98@hotmail.com>
2022-09-22 17:28:47 -0300

    feat: se agrega property en clase chinpokomon

--- chinpokomonPython/modelo/Chinpokomon.py
+++ chinpokomonPython/modelo/Chinpokomon.py
@@ -1,77 +1,76 @@
-from random import random
-
-
-class Chinpokomon():
+class Chinpokomon(object):
 
     # aca irian los atributos, agregar property
     def __init__(self):
-        self.nombre = None
-        self.vida = None
-        self.listaAtaques = []
-        self.naturaleza = None
-        self.random = None
-
-    @property
-    def random(self):
-        return self.random
-
-    @random.setter
-    def random(self, random):
-        self.random = random
+        self.__nombre= None
+        self.__vida = None
+        self.__listaAtaques = []
+        self.__naturaleza = None
+        self.__random = None
 
     @property
     def nombre(self):
-        return self.nombre
-
-    @nombre.setter
-    def nombre(self, nombre):
-        self.nombre = nombre
-
-    @property
-    def naturaleza(self):
-        return self.naturaleza
-
-    @naturaleza.setter
-    def naturaleza(self, naturaleza):
-        self.naturaleza = naturaleza
+      return self.__nombre
+    
+    @nombre.setter #Propiedad SETTER
+    def nombre(self, nuevo):
+        self.__nombre = nuevo
 
     @property
     def vida(self):
-        return self.vida
+      return self.__vida
 
     @vida.setter
-    def vida(self, vida):
-        self.vida = vida
-
+    def vida(self,nuevo):
+      self.__vida = nuevo
+    
     @property
     def listaAtaques(self):
-        return self.listaAtaques
+      return self.__listaAtaques
 
     @listaAtaques.setter
-    def listaAtaques(self, listaDeAtaques):
-        self.listaAtaques = listaDeAtaques
+    def listaAtaques(self,nuevo):
+      self.__listaAtaques = nuevo
+    
+    @property
+    def naturaleza(self):
+      return self.__naturaleza
 
+    @naturaleza.setter
+    def naturaleza(self,nuevo):
+      self.__naturaleza = nuevo
+
+    @property
+    def random(self):
+      return self.__random
+
+    @random.setter
+    def random(self,nuevo):
+      self.random = nuevo
+    
     def ataque(self, unChimpokomon):
-        if (self.noEstaMuertoOtroChimpokomon(unChimpokomon) and self.estoyVivo()):
+      if (self.noEstaMuertoOtroChimpokomon(unChimpokomon) and self.estoyVivo()):
             self.atacarSegunAtaqueSeleccionado(unChimpokomon)
 
     def atacarSegunAtaqueSeleccionado(self, chimpokomonAAtacar):
-        if(len(self.listaAtaques()) == 1):
-
+        if(self.cantidadDeAtaques == 1):
             ataque = self.listaAtaques()[0]
             ataque.generarEfecto(self, chimpokomonAAtacar)
         else:
             cantidadDeAtaques = len(self.listaAtaques())
-            rand = self.random()
+            rand = self.random
             ataqueElegido = rand.generarRandom(cantidadDeAtaques)
             ataque = self.listaAtaques()[ataqueElegido]
             ataque.generarEfecto(self, chimpokomonAAtacar)
 
     def estoyVivo(self):
-        return self.vida() > 0
+        return self.vida > 0
 
     def noEstaMuertoOtroChimpokomon(self, chimpokomon):
         return chimpokomon.vida() > 0
 
-    def toString(self):
-        return self.nombre() + " " + str(self.vida())
+    def tostring(self):
+        return self.nombre + " tiene de vida: " +  str(self.vida)
+
+    def cantidadDeAtaques(self):
+        return len(self.listaAtaques)
